4a7f96cd33528ec925d2c07da249a097b569f3da
Leif Anderson <leif.v.anderson@gmail.com>
2017-09-18 17:56:25 -0500

    angular registration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Registration';
+  user = { 'fname':'','lname':'', 'email':'', 'password':'', 'confirm':'',
+    'street':'', 'apt':'', 'city':'', 'state':'',  'lucky':'' };
+  users = [];
+
+  onSubmit(){
+    this.users.push(this.user);
+    this.user = { 'fname':'', 'lname':'', 'email':'', 'password':'', 'confirm':'',
+      'street':'', 'apt':'', 'city':'', 'state':'', 'lucky':'' };
+  }
 }
